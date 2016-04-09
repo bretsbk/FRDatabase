77b75cfdffafefe24de9aab42f5418bd8d131df0
Bret Gaulin <=>
2016-04-09 15:03:34 -0400

    Cleaner version. Also dynamic system name recognition for db creation on local server.

--- create_db_table.py
+++ create_db_table.py
@@ -1,17 +1,24 @@
 ﻿import pyodbc
+import socket
+import time
+
+system_name = str(socket.gethostname())
+db_name = 'NameFreq'
 
 def create_namefreq_db():
-    dbcon_str_setup = 'DRIVER={SQL Server};SERVER=HAL-9000\SQLEXPRESS'
+    dbcon_str_setup = 'DRIVER={SQL Server};SERVER=' + system_name + '\SQLEXPRESS'
     query_impltrans_off = 'SET IMPLICIT_TRANSACTIONS OFF;'
     query_createdb_create = 'CREATE DATABASE NameFreq;'
     db_con_db = pyodbc.connect(dbcon_str_setup)
     concurs = db_con_db.cursor()
     concurs.execute(query_impltrans_off)
+    concurs.commit()
+    time.sleep(3)
     concurs.execute(query_createdb_create)
     concurs.close()
 
 def create_namefreq_tables():
-    dbcon_str_name = 'Driver={SQL Server};Server=HAL-9000\SQLEXPRESS;DATABASE=NameFreq'
+    dbcon_str_name = 'Driver={SQL Server};Server=' + system_name + '\SQLEXPRESS' + ';DATABASE=' + db_name
     query_impltrans_off = 'SET IMPLICIT_TRANSACTIONS OFF'
     query_createtables = """
         CREATE TABLE NameFirstMale (
@@ -41,5 +48,6 @@ def create_namefreq_tables():
     db_con_table = pyodbc.connect(dbcon_str_name)
     concurs = db_con_table.cursor()
     concurs.execute(query_impltrans_off)
+    concurs.commit()
     concurs.execute(query_createtables)
     concurs.close()
